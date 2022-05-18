441b930c68bc735e646311aa827a2be65f5a97a7
pskrzypek <91827235+SkrzypekP@users.noreply.github.com>
2022-05-18 18:17:05 +0200

    Initial Commit

---
+++ unit conversions/unit_conversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  unit_conversionsApp.swift
+//  unit conversions
+//
+//  Created by Patryk on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct unit_conversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
